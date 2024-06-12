1d57cf214c7fc98dd07a8ad66bb712461c4db7a6
Luis Cid <alexcid2002@gmail.com>
2024-06-12 15:43:47 +0100

    us-states-game V1.2
    The game can now read the .csv file and store its data.
    
    Using this data we can now check if the user guess is a valid one, by checking the data we read from the file

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import turtle
+import pandas
 
 screen = turtle.Screen()
 screen.title("US States Game")
@@ -6,5 +7,20 @@ image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 
+data = pandas.read_csv("50_states.csv")
+all_states = data.state.to_list()
+guessed_states = []
 
-answer_state = screen.textinput(title="Guess the state", prompt="Whats another state name?")
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct",
+                                    prompt="What's another state's name?").title()
+
+
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(answer_state)
